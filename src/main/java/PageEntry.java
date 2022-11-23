0d32ba0dbc0454179429db7f80e045174d0e783f
IgorGultyaev <igor.gultyaev@mail.ru>
2022-11-23 22:38:47 +0300

    begin

--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,20 @@
+import java.util.Locale;
+
 public class PageEntry implements Comparable<PageEntry> {
     private final String pdfName;
     private final int page;
     private final int count;
 
-    // ???
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName.toLowerCase();
+        this.page = page;
+        this.count = count;
+    }
+
+    @Override
+    public int compareTo(PageEntry o) {
+
+        //описать сравнение
+        return 0;
+    }
 }
